c745c0c02398de8193026193fd9806904c3c2b58
BreFranca <bre.sfranca@gmail.com>
2018-09-20 22:16:25 -0300

    Finishing signature to comprebem

--- assets/main.js
+++ assets/main.js
@@ -4,14 +4,14 @@ function exportImage(nameArchive) {
 
 	    var base64image = canvas.toDataURL("image/png");
 
-	    var a = $("<a>").attr("href", base64image).attr("download", nameArchive+".png").appendTo("body");
+	    var a = $("<a>").attr("href", base64image).attr("download", 'assinatura_email_'+nameArchive+".png").appendTo("body");
 		a[0].click();
 		a.remove();
 	});
 }
 
 var SPMaskBehavior = function (val) {
-  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+  return val.replace(/\D/g, '').length === 11 ? '(00) 00000 0000' : '(00) 0000 00009';
 },
 spOptions = {
   onKeyPress: function(val, e, field, options) {
@@ -21,71 +21,61 @@ spOptions = {
 
 $('.phone').mask(SPMaskBehavior, spOptions);
 
-$('#input-name').keyup(function(event) {
-	event.preventDefault();
-	val = $(this).val();
-	$('.container-signature').fadeIn();
-	$('#name').html(val);
+$('#tabs .tab').click(function(e) {
+	var tab = $(this).attr('data-type');
+	$(tab).hide();
+	console.log(tab);
+	$('.tab-cont').show();
+	$('.tab-cont:not(".'+tab+'")').hide();
+	$('.container-signature').removeClass('layout-loja layout-escritorio')
+	$('.container-signature').addClass('layout-'+tab)
+})
+
+
+
+
+$('input').each(function(e) {
+
+	$(this).keyup(function(event) {
+		var name = $(this).attr('name');
+		var val = $(this).val();
+		$('#'+name).html(val);
+		$(this).removeClass('error');
+	});
 });
-
-$('#input-phone1').keyup(function(event) {
-	event.preventDefault();
-	val = $(this).val();
-	$('.container-signature').fadeIn();
-	$('#phone1').html(val);
-});
-
-$('#input-phone2').keyup(function(event) {
-	event.preventDefault();
-	val = $(this).val();
-	$('.container-signature').fadeIn();
-	$('#phone2-container').show();
-	$('#phone2').html(val);
-	if(val === '' || val === '(') {
-		$('#phone2-container').hide();
-	} 
-});
-
-$('#input-email').keyup(function(event) {
-	event.preventDefault();
-	val = $(this).val();
-	$('.container-signature').fadeIn();
-	$('#email').html(val);
-});
-
-// $('#input-address').keyup(function(event) {
-// 	event.preventDefault();
-// 	val = $(this).val();
-// 	$('#address').html(val);
-// });
-
-// $('#input-site').keyup(function(event) {
-// 	event.preventDefault();
-// 	val = $(this).val();
-// 	$('#site').html(val);
-// });
 
 
 $('button').click(function(e){
 	e.preventDefault();
-	var name = $('#name').html(),
-		email = $('#email').html(),
-		phone1 = $('#phone1').html(),
-		phone2 = $('#phone2').html(),
-		// address = $('#address').html(),
-		// site = $('#site').html(),
-
+		var name = $('#nome-completo').html()
 		nameArchive = name.replace(/ /g, '-');
 		nameArchive = nameArchive.toLowerCase();
 
-	if($.isNumeric(name)) {
-		alert('Nome pode conter somente letras');
-	} else {
-		if(name === '' || email === '' || phone1 === '' || name === '' || name === '') {
-			alert('Existem campos em branco');
-		} else {
-			exportImage(nameArchive);
-		}
-	}
+		$('input').removeClass('success error');
+		$('input').each(function(e) {
+			if($(this).val() === '') {
+				$(this).addClass('error');
+			} else {
+				var regex = /@comprebem\.com\.br$/;
+				var email = $('#input-email').val();
+				test = regex.test(email);
+				if($(this).attr('name') === 'nome-completo' || $(this).val() === '') {
+					if($.isNumeric($(this).val())) {
+						alert('Nome pode conter somente letras');
+						$(this).addClass('error');
+					} else {
+						$(this).addClass('success');
+					}
+				} else if(!test) {
+					$('#input-email').addClass('error');
+					alert('Somente email corporativo');
+				} else {
+					$(this).addClass('success');
+					exportImage(nameArchive);
+				}
+
+
+			}
+		});
 
 });
